6a981e7b3c74f6d1f49525feb0c1fec02a7ca59a
jiyeon <alclsemfja@naver.com>
2021-10-01 18:14:10 +0900

    아무거나

--- src/project/dao/ProjectDAO.java
+++ src/project/dao/ProjectDAO.java
@@ -1,5 +1,17 @@
 package project.dao;
 
 public class ProjectDAO {
-
+  static {
+	  try {
+		
+		  Calss.forName("oracle.jdbc.OracleDriver");
+	} catch (Exception e) {
+		// TODO: handle exception
+	}
+		  
+	  
+  }
+	
+	
+	
 }
