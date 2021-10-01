fc8fa6ddd5577ba3721934c29213cd96309996cd
JEONHANYOUNG <jupiter12345@naver.com>
2021-10-01 18:18:28 +0900

    dao 생성

--- src/project/dao/ProjectDAO.java
+++ src/project/dao/ProjectDAO.java
@@ -1,5 +1,16 @@
 package project.dao;
 
 public class ProjectDAO {
-
+	
+	
+	static {
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+	} 
+	
+	
 }
