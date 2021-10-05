aa3da968348c431ffde31e9c888370ebf3700939
jiyeon <alclsemfja@naver.com>
2021-10-05 10:43:00 +0900

    메인수정

--- src/project/dao/ProjectDAO.java
+++ src/project/dao/ProjectDAO.java
@@ -43,7 +43,8 @@ public class ProjectDAO {
 	   
 		   
 	   }
-  }
+	   }
+		
 
 
 
--- src/project/dao/ProjectMain.java
+++ src/project/dao/ProjectMain.java
@@ -3,7 +3,7 @@ package project.dao;
 public class ProjectMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("こんにちは");
 
 	}
 
