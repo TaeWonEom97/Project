49ad11385e223ddd2d952374eb80134b76d42575
jaelim <jaelimly@gmail.com>
2021-10-05 10:30:59 +0900

    DTO 작성

---
+++ src/project/dao/ProjectDTO.java
@@ -0,0 +1,26 @@
+package project.dao;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProjectDTO {
+	private int empno;
+	private String ename;
+	private String job;
+	private int mgr;
+	private Date hiredate;
+	private int sal;
+	private int comm;
+	private int deptno;
+	
+}
