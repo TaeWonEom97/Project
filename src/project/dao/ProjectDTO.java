36169c97b0df926ce158ac5201be13e3d71e5e16
사용자명 <jupiter12345@naver.com>
2021-10-05 10:02:58 +0900

    코드 생성

---
+++ src/project/dao/ProjectDTO.java
@@ -0,0 +1,11 @@
+package project.dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Collection;
+
+public class ProjectDTO {
+	 Collection con = null;
+	 PreparedStatement pstmt = null;
+	 ResultSet rs = null;
+}
