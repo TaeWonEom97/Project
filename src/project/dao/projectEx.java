cba2b02e16e8c35824a4406c8c398dbd4a62eef0
won <utw1960@naver.com>
2021-10-05 10:01:05 +0900

    예제

--- src/project/dao/projectEx.java
+++ src/project/dao/projectEx.java
@@ -1,5 +1,17 @@
 package project.dao;
 
 public class projectEx {
-
+	static {
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+	}
 }
